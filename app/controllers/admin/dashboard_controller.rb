86f7b0f55d4169ad182bc26651473d159eb0dc18
dimontejulia <dimontejulia@gmail.com>
2021-01-06 16:44:27 -0500

    adding basic auth to all admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV["AUTHENTICATION_USER"], password: ENV["AUTHENTICATION_PASSWORD"], if: -> { ENV["AUTHENTICATION_PASSWORD"].present?}
+  http_basic_authenticate_with name: ENV["AUTHENTICATION_USER"], password: ENV["AUTHENTICATION_PASSWORD"]
 
   def show
   end
