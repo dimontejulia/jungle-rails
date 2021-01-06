01000d06df49e2fcc4ed815d0005ebc77e9fc8cc
dimontejulia <dimontejulia@gmail.com>
2021-01-06 16:33:58 -0500

    added username & password and added authentication to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["AUTHENTICATION_USER"], password: ENV["AUTHENTICATION_PASSWORD"], if: -> { ENV["AUTHENTICATION_PASSWORD"].present?}
+
   def show
   end
 end
