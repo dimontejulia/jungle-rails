9e44f280e2e14a450493ef487f724e367eda73d4
dimontejulia <dimontejulia@gmail.com>
2021-01-06 17:44:06 -0500

    added number of products

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,6 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["AUTHENTICATION_USER"], password: ENV["AUTHENTICATION_PASSWORD"]
 
   def show
+    @products = Product.order(id: :desc).all
   end
 end
