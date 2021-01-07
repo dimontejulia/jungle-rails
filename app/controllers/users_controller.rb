db70611d2e572e06ff1f14f06ac41601f360bab9
dimontejulia <dimontejulia@gmail.com>
2021-01-07 15:36:01 -0500

    fixing bug with conditional logic on login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,15 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/'
+      flash[:notice] = 'Account created successfully'
+      redirect_to :root 
     else
-      redirect_to '/signup'
+      flash[:error] = 'An error occured signing up.'
+      redirect_to :signup
     end
   end   
+
+  private 
 
   def user_params
     params.require(:user).permit(:first_name, :last_name ,:email, :password, :password_confirmation)
