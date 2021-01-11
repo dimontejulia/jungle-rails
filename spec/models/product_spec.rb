3d434d2aa8d74e8568e78ec4d58715bab39b4a2e
dimontejulia <dimontejulia@gmail.com>
2021-01-11 14:03:50 -0500

    initial test - all valid inputs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+
+  
+
   describe 'Validations' do
-    # validation tests/examples here
+    it "should save a product with valid attributes" do 
+      @category = Category.create(name:"Magic")
+      product = Product.new(
+        name: "Magic Want",
+        price: 66.00,
+        quantity: 21,
+        category_id: @category.id
+        )
+      expect(product).to (be_valid)
+    end
+    
   end
 end
