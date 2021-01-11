3e215a366678c4453222f1187bafebabdcc96369
dimontejulia <dimontejulia@gmail.com>
2021-01-11 14:18:13 -0500

    added 3 other validation tests for empty inputs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -16,18 +16,45 @@ RSpec.describe Product, type: :model do
       expect(product).to (be_valid)
     end
     it "is not valid without a name" do 
-    @category = Category.create(name:"Magic")
-    product = Product.new(
-      name: nil,
-      price: 66.00,
-      quantity: 21,
-      category_id: @category.id
-      )
-    expect(product).to_not (be_valid)
+      @category = Category.create(name:"Magic")
+      product = Product.new(
+        name: nil,
+        price: 66.00,
+        quantity: 21,
+        category_id: @category.id
+        )
+      expect(product).to_not (be_valid)
+    end
+    it "is not valid without a price" do
+      @category = Category.create(name:"Magic")
+      product = Product.new(
+        name: "Cloak",
+        price: "",
+        quantity: 20,
+        category_id: @category.id
+        )
+      expect(product).to_not (be_valid)
+    end
+    it "is not valid without a quantity" do
+      @category = Category.create(name:"Magic")
+      product = Product.new(
+        name: "Broomstick",
+        price: "355.00",
+        quantity: nil,
+        category_id: @category.id
+        )
+      expect(product).to_not (be_valid)
+    end
+    it "is not valid without a category" do
+      @category = Category.create(name:"Magic")
+      product = Product.new(
+        name: "Potion vials",
+        price: 120.99,
+        quantity: 120,
+        category_id: nil
+        )
+      expect(product).to_not (be_valid)
   end
 
-    # it "is not valid without a description"
-    # it "is not valid without a start_date"
-    # it "is not valid without a end_date"
   end
 end
