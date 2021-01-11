ae510725481fa41557bfc69520e255749c693cf2
dimontejulia <dimontejulia@gmail.com>
2021-01-11 14:06:42 -0500

    test: name set to nil, is not valid

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,13 +8,26 @@ RSpec.describe Product, type: :model do
     it "should save a product with valid attributes" do 
       @category = Category.create(name:"Magic")
       product = Product.new(
-        name: "Magic Want",
+        name: "Magic Wand",
         price: 66.00,
         quantity: 21,
         category_id: @category.id
         )
       expect(product).to (be_valid)
     end
-    
+    it "is not valid without a name" do 
+    @category = Category.create(name:"Magic")
+    product = Product.new(
+      name: nil,
+      price: 66.00,
+      quantity: 21,
+      category_id: @category.id
+      )
+    expect(product).to_not (be_valid)
+  end
+
+    # it "is not valid without a description"
+    # it "is not valid without a start_date"
+    # it "is not valid without a end_date"
   end
 end
